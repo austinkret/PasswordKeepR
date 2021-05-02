746598bd6af4c1d1793d3be732ae3a9c1360de03
austinkret <austinkret@gmail.com>
2021-05-02 16:38:44 +0000

    removed conditional session validation

--- routes/welcome-page.js
+++ routes/welcome-page.js
@@ -3,9 +3,6 @@ const router  = express.Router();
 
 module.exports = () => {
   router.get("/", (req, res) => {
-    if (req.session.test) {
-      return res.sendFile(path.join(__dirname + "/public/home.html"));
-    }
     return res.render('welcome-page');
   });
   return router;
